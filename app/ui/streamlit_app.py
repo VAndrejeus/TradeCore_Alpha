73b775613cc0f55f0b7a04876a10dcde81609939
Vitaly Andrejeus <andrejeusv@gmail.com>
2026-04-23 07:15:03 -0700

    Added watchlist

--- app/ui/streamlit_app.py
+++ app/ui/streamlit_app.py
@@ -2,7 +2,15 @@ import streamlit as st
 
 st.title("TradeCore Alpha")
 
+if "watchlist" not in st.session_state:
+    st.session_state.watchlist = []
+
 symbol = st.text_input("Enter a stock symbol")
 
 if st.button("Add symbol"):
-    st.write("Added", symbol)
+    clean_symbol = symbol.upper().strip()
+
+    if clean_symbol and clean_symbol not in st.session_state.watchlist:
+        st.session_state.watchlist.append(clean_symbol)
+st.write("Current watchlist:")
+st.write(st.session_state.watchlist)
